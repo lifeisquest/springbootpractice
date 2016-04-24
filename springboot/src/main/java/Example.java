ab43cba64c79a64156347c2f6a8a1e0d7cb1df6f
steven <grapheople@gmail.com>
2016-04-24 16:36:06 +0900

    set parent project & module

---
+++ springboot/src/main/java/Example.java
@@ -0,0 +1,23 @@
+package com.grapheople.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by grapheople on 2016. 4. 22..
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "hello world!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
